2c29ddf5bd31e5eec3620412ea67a7d6c56810a4
Admin <admin@local>
2026-04-29 07:39:35 +0300

    Final merge and  update platform admin

--- task_manage/settings.py
+++ task_manage/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'testserver']
 # APPLICATIONS
 # =========================
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
